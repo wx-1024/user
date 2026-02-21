ccbd9e210ef072ec405197040fe4af101bb93d8d
CaptainTeemo <teemo@CaptainTeemodeMacBook-Pro.local>
2026-02-21 20:59:08 +0800

    chore: change dev port to 3751, proxy to 8800

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0', // 监听所有网络接口
-    port: 5173,
+    port: 3751,
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8800',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8800',
         changeOrigin: true,
       }
     }
